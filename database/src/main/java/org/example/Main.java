874e801604ce1a162eed5a024315ea3df036c7e3
yinhd <yinhd@goldencis.com>
2023-08-13 16:35:00 +0800

    jap连接sqlite数据库

---
+++ database/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: Default (Template) Project
+ * @description:
+ * @Author: yinhd
+ * @create: 2023-07-28 17:22
+ **/
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
