f685eee7f8c5b5e5333b5ddb0b376114269330f4
yinhd <yinhd@goldencis.com>
2023-08-23 09:32:48 +0800

    日拱一卒

---
+++ archApawn/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @program: Default (Template) Project
+ * @description:
+ * @Author: yinhd
+ * @create: 2023-08-22 14:03
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
